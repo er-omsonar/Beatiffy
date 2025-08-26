66dd52d0b74aa974f069a649e6872d7d578a09fb
er-omsonar <er.omsonar@gmail.com>
2025-08-26 19:18:26 +0530

    making the song list section good and also choose icons and font style for project

--- script.js
+++ script.js
@@ -20,12 +20,25 @@ async function main() {
   console.log(songs);
   let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
   for (song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20"," ")
+    songUL.innerHTML = songUL.innerHTML +
+    
+    
+    `<li>
+                <img class="invert" src="music.svg" alt="">
+                <div class="info">
+                  <div>${song.replaceAll("%20"," ")
       .replaceAll("The Grey Room "," ")
       .replaceAll(".mp3"," ")
       .replaceAll("%26"," ")
-    } </li>`;
-  }
+    }</div>
+                  <div>harry</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="play.svg" alt="">
+                </div>
+              </li>`
+    } 
   var audio = new Audio(songs[0]);
 //  audio.play();
 }
