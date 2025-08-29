07dfe2390a2da695a591c176bfd0b33da0e68a72
er-omsonar <er.omsonar@gmail.com>
2025-08-29 17:34:12 +0530

    Building a hamburger functionality for mobile devices.

--- script.js
+++ script.js
@@ -106,6 +106,14 @@ async function main() {
       currentSong.currentTime = ((currentSong.duration) * percent)/100
       
     })
+
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+      document.querySelector(".left").style.left = "0"
+    })
+
+    document.querySelector(".close").addEventListener("click",()=>{
+      document.querySelector(".left").style.left = "-120%"
+    })
 }
 
 main(); 
